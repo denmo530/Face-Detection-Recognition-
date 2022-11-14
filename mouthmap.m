52fb54798707744fea8a3237a33cbe0c5898caaa
richardgotthard <richard.gotthard@hotmail.com>
2022-11-14 10:37:56 +0100

    mouthmap and eyemap fix

--- mouthmap.m
+++ mouthmap.m
@@ -2,21 +2,32 @@ function [result] = mouthmap(input)
 %Generates mouthmap from image.
 
 %Convert Image
-img_ycbcr = rgb2ycbcr(input);
-img_ycbcr = im2double(img_ycbcr);
+img_ycbcr = (rgb2ycbcr(input));
 
-Y = img_ycbcr(:,:,1);
-Cb = img_ycbcr(:,:,2);
-Cr = img_ycbcr(:,:,3);
-%[Y, Cb, Cr] = imsplit(img_ycbcr);
-
+Y = mat2gray(img_ycbcr(:,:,1)) +1;
+Cb = mat2gray(img_ycbcr(:,:,2)) +1;
+Cr = mat2gray(img_ycbcr(:,:,3)) +1;
 
 %eta = η
-n = numel(input);
-eta = 0.95 * ((1/n) * sum(sum(Cr.^2)))/((1/n)*sum(sum(Cr./Cb)));
+%n = numel(input);
+
+eta = 0.95 * (mean(Cr(:).^2))/(mean(Cr(:)./Cb(:)));
+
 
 %TODO Mouthmap doesnt seem to work atm
 %MouthMap:
-result = (Cr.^2) .* (Cr.^2 - eta * (Cr./Cb)).^2;
+result = (Cr.^2).*((Cr.^2) - eta*(Cr./Cb)).^2;
+
+%result = histeq(result);
+
+result = (result- min(result(:)))/(max(result(:)) - min(result(:)));
+
+%se1 = strel('disk', 12);
+
+
+%result = result > 0.2;
+
+%result = bwareaopen(result, 200);
+%result = imdilate(result, se1);
 
 end
