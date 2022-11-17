ea72e136380a493b924fbc23e476bbafed5f50ae
Dennis <dennis.moradkhani@gmail.com>
2022-11-17 14:38:58 +0100

    Facemask implemented, somewhat good results, could be improved upon

--- eyemapC.m
+++ eyemapC.m
@@ -3,17 +3,16 @@ function [rgbImg] = eyemapC(input)
 img_ycbcr = rgb2ycbcr(input);
 
 %[Cr, ~, Cb] = imsplit(img_ycbcr);
-img = im2double(img_ycbcr);
-Y = img(:,:,1);
-Cb = img(:,:,2);
-Cr = img(:,:,3);
+%img = im2double(img_ycbcr);
+Y = uint16(255 * mat2gray(img_ycbcr(:,:,1)));
+Cb = uint16(255 * mat2gray(img_ycbcr(:,:,2)));
+Cr = uint16(255 * mat2gray(img_ycbcr(:,:,3)));
 
 % Chromincance eyemap
-eyeMapC = ((Cb.^2)+((1-Cr).^2)+(Cb./Cr))/3; 
+eyeMapC = im2double(((Cb.^2)+((1-Cr).^2)+(Cb./Cr))./3); 
 
 %Histogram normalization
-normImg = histeq(eyeMapC)
+normImg = histeq(eyeMapC);
 rgbImg = normImg; 
-
 
 end
