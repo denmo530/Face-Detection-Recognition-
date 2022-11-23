51022e95f09f70645075f30a0c3a473a6341a86e
Dennis <dennis.moradkhani@gmail.com>
2022-11-23 11:16:59 +0100

    Eyemap looks good now

--- eyemapC.m
+++ eyemapC.m
@@ -3,13 +3,13 @@ function [rgbImg] = eyemapC(input)
 img_ycbcr = rgb2ycbcr(input);
 
 %[Cr, ~, Cb] = imsplit(img_ycbcr);
-%img = im2double(img_ycbcr);
-Y = uint16(255 * mat2gray(img_ycbcr(:,:,1)));
-Cb = uint16(255 * mat2gray(img_ycbcr(:,:,2)));
-Cr = uint16(255 * mat2gray(img_ycbcr(:,:,3)));
+img_ycbcr = im2double(img_ycbcr);
+Y = img_ycbcr(:,:,1);
+Cb = img_ycbcr(:,:,2);
+Cr = img_ycbcr(:,:,3);
 
 % Chromincance eyemap
-eyeMapC = im2double(((Cb.^2)+((1-Cr).^2)+(Cb./Cr))./3); 
+eyeMapC = ((Cb.^2)+((1-Cr).^2)+(Cb./Cr))./3; 
 
 %Histogram normalization
 normImg = histeq(eyeMapC);
