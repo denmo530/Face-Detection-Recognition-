ee2b50edadf230181a8d0e4fc9829e68c1ebfd23
Beckelund <enjohan@hotmail.com>
2022-12-04 16:04:10 +0100

    merge with current version
    began implementing transformations for normalize face

--- normalizeface.m
+++ normalizeface.m
@@ -21,6 +21,8 @@ imshow(input);
 hold on
 plot(leftEye,rightEye, 'rx','MarkerSize', 15, 'LineWidth', 1);
 hold off
+plot(left_eye(1),left_eye(2), 'r+', 'MarkerSize', 15, 'LineWidth', 1);
+plot(right_eye(1),right_eye(2), 'r+', 'MarkerSize', 15, 'LineWidth', 1);
 
 %angle = atan2(right_eye(2) - left_eye(2), right_eye(1) - left_eye(1));
 %angle = angle * 180 / pi;
@@ -38,9 +40,29 @@ hold off
 %plot(176,279, 'r+', 'MarkerSize', 15, 'LineWidth', 1);
 
 
+plot(mouthCoor(1), mouthCoor(2), 'r+', 'MarkerSize', 15, 'LineWidth', 1);
 
 %Rotate Image
     %TODO rotate image based on eye positions.
+[image_height, image_width] = size(input(:,:,1))
+
+
+%Transform: translate and scale so that face covers entire picture
+left_eye_target = [20*image_width/100, 20*image_height/100]; % TODO maybe round these values
+right_eye_target = [80*image_width/100, 20*image_height/100];
+
+plot(left_eye_target(1),left_eye_target(2), 'b+', 'MarkerSize', 10, 'LineWidth', 1);
+plot(right_eye_target(1),right_eye_target(2), 'b+', 'MarkerSize', 10, 'LineWidth', 1);
+
+    %Translate for left eye
+translate_amount = left_eye_target - left_eye
+input = imtranslate(input,translate_amount);
+
+    %Scale for right eye
+%input = imtransform()
+
+hold off
+imshow(input)
 
 %Scale image to another resolution
     %TODO scale might be changed when previous steps are fixed
