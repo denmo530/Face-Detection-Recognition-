cf149fd2e91a8682562f1762c5725ef071cc2411
Beckelund <enjohan@hotmail.com>
2022-12-06 08:55:15 +0100

    Allows you to save all normalized images as file

--- loaddatabase.m
+++ loaddatabase.m
@@ -3,18 +3,27 @@ function [result] = loaddatabase(db_id)
 
 folder = "Images";
 
-if db_id == 1
-    folder = folder + "/DB1/";
+should_update = true;
 
-    %Pre-allocate size (needs to match normalized image)
-    result = zeros(400,300,16);
-
+if should_update == false
+    S = load("SavedData/normalizedfaces.mat", "faces");
+    result = S.faces;
+else
+    if db_id == 1
+        folder = folder + "/DB1/";
+    
+        %Pre-allocate size (needs to match normalized image)
+        result = zeros(400,300,16);
+    end
     for i = 1:1:16
         path = folder + "db1_" + sprintf('%02d',i) + ".jpg";
         current_image = imread(path);
+        disp(i)
         current_image = normalizeface(current_image);
         result(:,:,i) = current_image(:,:);
     end
+    faces = result;
+    save("SavedData/normalizedfaces.mat", "faces");
 end
 
 
