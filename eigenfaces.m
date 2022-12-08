681c5c0a48a18823d33aa334819a76c24229deea
Beckelund <enjohan@hotmail.com>
2022-12-08 13:22:03 +0100

    rename variables

--- eigenfaces.m
+++ eigenfaces.m
@@ -1,4 +1,4 @@
-function [result meanface] = eigenfaces(input)
+function [u_i meanface] = eigenfaces(input)
 %Create eigenface from a set of images that is normalized in rotation, scaling &
 %tonevalues. The image will be the same size as the other images.
 
@@ -31,13 +31,12 @@ v = transpose(A)*A;
 % 6c.
 u_i = A*v;
 
-for i = 1:1:M
-    normalize = mat2gray(u_i);
-    imshow(reshape(normalize(:,i),400,[]))
-end
+% for i = 1:1:M
+%     normalize = mat2gray(u_i);
+%     imshow(reshape(normalize(:,i),400,[]))
+% end
 
 save('SavedData/u_i.mat', "u_i","u")
-result = u_i;
 meanface = u;
 
 end
