3c9cd165f164a256520131999530a4d44e2a6542
Beckelund <enjohan@hotmail.com>
2022-12-13 16:48:37 +0100

    function to run for final result

--- tnm034.m
+++ tnm034.m
@@ -7,4 +7,41 @@ function id = tnm034(im)
 % i.e. ‘1’, ‘2’,...,‘16’ for the persons belonging to ‘db1’ % and ‘0’ for all other faces.
 %% Your program code
 
+%Add seperate path
+addpath(genpath("facefunctions"));
+addpath(genpath("eigenfacefunctions"));
+
+%Normalize input image
+    normalized_image = normalizeface(im);
+
+%Get feature vector of input
+    %Load eigenfaces form database (DB1)
+    S = load('SavedData/u_i.mat', "u_i","u");
+    u_i = [S(:).u_i];
+    meanface = [S(:).u];
+
+    %Generate feature vector
+    input_featureVector = getFeatureVector(normalized_image, u_i, meanface);
+
+
+%Compare input feature vector to database
+    %Load feature vectors from database
+    S = load("SavedData/featurevectors.mat", "featureVectors");
+    featureVectors = [S(:).featureVectors];
+
+    %Find shortest distance (best match)
+    distances = zeros(1,size(featureVectors,1));    %Pre allocate
+    for j = 1:16
+        distances(:,j) = norm(input_featureVector(:) - featureVectors(:,j));
+    end
+    shortest = min(min(distances));
+
+%Return -1 if image is not in database
+    no_match_threshold = 5.0e+06;
+    
+    if(shortest > no_match_threshold)
+        id = -1;
+    else
+        id = find(distances == min(min(distances)));
+    end
 end
